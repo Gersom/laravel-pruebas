1ec6f7df40b6211a0380a41ca93c8102153957bb
gersom <gersomalaja@gmail.com>
2017-06-15 00:46:53 -0500

    [laravel/migrations] added & update users, products, images, people, comapnies

--- database/migrations/2017_06_14_231756_create_products_table.php
+++ database/migrations/2017_06_14_231756_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->string('name')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('role'); //1=administrator 2=company 3=person
+            $table->string('code');
+            $table->text('description');
+            $table->string('name');
+            $table->float('price');
+            $table->boolean('published');
 
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
--- database/migrations/2017_06_15_002828_create_images_table.php
+++ database/migrations/2017_06_15_002828_create_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->string('name')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('role'); //1=administrator 2=company 3=person
+            $table->text('original');
+            $table->text('height_50px');
+            $table->text('height_150px');
+            $table->text('height_500px');
 
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('images');
     }
 }
